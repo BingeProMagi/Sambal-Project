8174e2d8581c98cdadb760599202b709314870cd
Markus Pantzar <Markus.pantzar@gmail.com>
2016-12-20 17:50:17 +0100

    First Commit

---
+++ _site/assets/js/common.js
@@ -0,0 +1,17 @@
+(function ($){
+	var $mobileNavToggleBtn = $('.mobile-nav-toggle');
+	
+	function onBtnClick(e) {
+		var $this = $(this),
+			$selectors = $('.mobile-nav');
+
+		
+		$this.toggleClass('is-open');
+		$selectors.toggleClass('is-open');
+	}
+
+	$(document).ready(function (){
+		$mobileNavToggleBtn.on('click', onBtnClick);	
+	});
+
+})(jQuery);
