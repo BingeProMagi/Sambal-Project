7da210eb1713f3b2a5a5cf4c24f41dab71171dda
BingeProMagi <markus.pantzar@gmail.com>
2016-12-26 16:41:01 +0100

    börjat med javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,26 @@
-console.log("hej Mamma");
+
+	// Global variables  	
+var playing = [hannes, sandra, malin, markus, gustav, rangvi];
+
+	// This is the DOM-Ready
+$(function(){
+
+	for(var i = 0; i > playing.length; i++){
+		playing.push(new Players( ( i )));
+		
+	};
+	console.log(playing);
+
+
+});
+
+
+// Creating player object
+function Players(name, castle, wins, lastWin, currentTeam){
+	this.name = name;
+	this.castle = castle;
+	this.wins = wins;
+	this.lastWin = lastWin;
+	this.currentTeam = currentTeam;
+}
+
