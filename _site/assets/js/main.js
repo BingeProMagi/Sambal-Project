1ed949f4662128ed79318e36c8c33281eef692b6
BingeProMagi <markus.pantzar@gmail.com>
2016-12-30 23:59:31 +0100

    popup och bakgrundsbild, buttons och diverse js

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -1,26 +1,34 @@
 
 	// Global variables  	
-var playing = [hannes, sandra, malin, markus, gustav, rangvi];
-
+var playing = ["Hannes", "Sandra", "Malin", "Markus", "Gustav", "Rangvi"];
+var play = [];
 	// This is the DOM-Ready
 $(function(){
 
-	for(var i = 0; i > playing.length; i++){
-		playing.push(new Players( ( i )));
+	console.log(playing[1])
+	for(var i = 0; i < playing.length; i++){
 		
+		play[i] = new Players(playing[i]);
+	
 	};
-	console.log(playing);
-
-
+	console.log(play);
+	$('#start').click(function(){
+		$('#battle-background').show();
+  		$('#battle').show();
+	});
+	$('#end').click(function(){
+		$(this).parent().hide();
+	  	$('#battle-background').css('display','none');
+	});
 });
 
 
 // Creating player object
-function Players(name, castle, wins, lastWin, currentTeam){
+function Players(name){
 	this.name = name;
-	this.castle = castle;
+	/*this.castle = castle;
 	this.wins = wins;
 	this.lastWin = lastWin;
-	this.currentTeam = currentTeam;
+	this.currentTeam = currentTeam;*/
 }
 
